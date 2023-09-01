0675fa0f44041792ebf01dacfb892cc4eb6d049a
chosunghyun18 <krismon@naver.com>
2023-09-01 20:50:50 +0900

    feat : add save first entity when get input name

--- src/main/java/menu/back/app/coach/application/service/AddCoachNameService.java
+++ src/main/java/menu/back/app/coach/application/service/AddCoachNameService.java
@@ -5,19 +5,22 @@ import menu.back.app.coach.adapter.persistence.CoachPersistenceAdapter;
 import menu.back.app.coach.adapter.presentation.AddCoachNameRequest;
 import menu.back.app.coach.application.port.in.AddCoachNameUseCase;
 import menu.back.app.coach.application.port.out.LoadCoachPort;
+import menu.back.app.coach.application.port.out.SaveCoachPort;
 import menu.back.app.coach.converter.CoachConverter;
 import menu.back.app.coach.domain.Coach;
 
 public class AddCoachNameService implements AddCoachNameUseCase {
     private final LoadCoachPort loadCoachPort;
-
+    private final SaveCoachPort saveCoachPort;
     public AddCoachNameService() {
         this.loadCoachPort = new CoachPersistenceAdapter();
+        this.saveCoachPort = new CoachPersistenceAdapter();
     }
-
+    // 이름만 저장을해야 하지만 시스템상 어쩔수 없이 코치 엔터티를 같이 생성하는 함수의 형태
     @Override
     public void add(AddCoachNameRequest addCoachRequest) {
         List<String> names =CoachConverter.getCoachNameFromRequest(addCoachRequest);
+        saveCoachPort.add(new Coach());
         Coach coach = loadCoachPort.getCoach();
         coach.storeCoaches(names);
     }
--- src/main/java/menu/back/app/menu/adapter/in/presentation/RecommendController.java
+++ src/main/java/menu/back/app/menu/adapter/in/presentation/RecommendController.java
@@ -7,38 +7,38 @@ public class RecommendController {
     public RecommendController() {
 
     }
-    public RecommendInfoResponse getRecommendMenu() {
-//        recommendCategory();
-//        recommendMenu();
-        for (int i = 0; i < 5; i++) {
-            String category = categories.getCategoriesByDay(i);
-            List<String> menusAll = menu.getOriginMenuByCategory(category);
-            for (int j = 0; j < numberOfCoaches; j++) {
-                boolean selceted = false;
-                List<String> dislikes = coaches.getCoachesDislikeByOrder(j);
-                while (!selceted) {
-                    String menuSelected = menu.getMenuFromMenus(removeDislike(menusAll, dislikes));
-                    if (storeSelected(menuSelected, j)) {
-                        selceted = true;
-                    }
-                }
-            }
-        }
-    }
-    private List<String> removeDislike(List<String> menusAll, List<String> dislikes) {
-        for (String menuDilike : dislikes) {
-            menusAll.remove(menuDilike);
-        }
-        return menusAll;
-    }
-
-    private boolean storeSelected(String menuSelected, int j) {
-        String name = coaches.getCoachesName(j);
-        if (coaches.isSelected(menuSelected, name)) {
-            return false;
-        }
-        coaches.storeRecommand(menuSelected, name);
-        return true;
-    }
+//    public RecommendInfoResponse getRecommendMenu() {
+////        recommendCategory();
+////        recommendMenu();
+//        for (int i = 0; i < 5; i++) {
+//            String category = categories.getCategoriesByDay(i);
+//            List<String> menusAll = menu.getOriginMenuByCategory(category);
+//            for (int j = 0; j < numberOfCoaches; j++) {
+//                boolean selceted = false;
+//                List<String> dislikes = coaches.getCoachesDislikeByOrder(j);
+//                while (!selceted) {
+//                    String menuSelected = menu.getMenuFromMenus(removeDislike(menusAll, dislikes));
+//                    if (storeSelected(menuSelected, j)) {
+//                        selceted = true;
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    private List<String> removeDislike(List<String> menusAll, List<String> dislikes) {
+//        for (String menuDilike : dislikes) {
+//            menusAll.remove(menuDilike);
+//        }
+//        return menusAll;
+//    }
+//
+//    private boolean storeSelected(String menuSelected, int j) {
+//        String name = coaches.getCoachesName(j);
+//        if (coaches.isSelected(menuSelected, name)) {
+//            return false;
+//        }
+//        coaches.storeRecommand(menuSelected, name);
+//        return true;
+//    }
 
 }
